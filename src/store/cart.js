233090aba5c35f3f35b2bfb488c551ccf0e9d366
inre25 <89619697979@mail.ru>
2025-09-15 23:06:55 +1200

    feat: cart store (zustand)

---
+++ src/store/cart.js
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+export const useCart = create((set, get) => ({
+  items: [],
+  addItem: (item) => {
+    const it = { qty: 1, ...item };
+    set({ items: [...get().items, it] });
+  },
+  clearCart: () => set({ items: [] }),
+}));
